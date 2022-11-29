21ac6fe8ac040dc32feb8fe8d6b7f8359e76e4a5
Daniel Marvin <532658+drippdropp@users.noreply.github.com>
2022-11-29 02:02:42 -0500

    include iterative version of gcd

--- includes/math.h
+++ includes/math.h
@@ -1,19 +1,59 @@
 #ifndef NTLIB_MATH_H
 #define NTLIB_MATH_H
 
+#define swap(p, q) { int temp = p; p = q; q = temp; }
+
 namespace ntlib {
+  
+   /**
+    * gcd (recursive version of gcd)
+    **/
+  long gcd(long a, long b) {
+    return b == 0 ? a : gcd(b, a % b);
+  }
 
-long gcd(long a, long b) {
-  return b == 0 ? a : gcd(b, a % b);
-}
+  /**
+   * gcd (recursive version of gcd)
+   **/
+  int gcd(int a, int b) {
+    return b == 0 ? a : gcd(b, a % b);
+  }
 
-int gcd(int a, int b) {
-  return b == 0 ? a : gcd(b, a % b);
-}
+  /**
+   * gcdi (iterative version of gcd)
+   **/
+  long gcdi(long a, long b) {
+    long gcd_output;
+    if (a > b) swap(a, b);
+    for (size_t i = 1; i <= b; ++i) {
+      if (a % i == 0 && b % i == 0) {
+        gcd_output = i;
+      }
+    }
+    return gcd_output;
+  }
 
-long lcm(long a, long b) {
-  return (a*b)/gcd(a, b);
-}
+  /**
+   * gcdi (iterative version of gcd)
+   **/
+  int gcdi(int a, int b) {
+    int gcd_output;
+    if (a > b) swap(a, b);
+    for (size_t i = 1; i <= b; ++i) {
+      if (a % i == 0 && b % i == 0) {
+        gcd_output = i;
+      }
+    }
+    return gcd_output;
+  }  
+  
+  long lcm(long a, long b) {
+    return (a*b)/gcd(a, b);
+  }
+  
+  long lcmi(long a, long b) {
+    return (a*b)/gcdi(a, b); 
+  }
 
 }
 
